5832e857070d66e3a2b13f3f85ef8ddf8c751ccb
Joseph Jean-Charles <joseph.jeancharles@outlook.com>
2025-03-11 17:48:44 -0400

    Add project files.

---
+++ Controls/TeamMate.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CSharp.UWP.App.Controls
+{
+    public sealed partial class TeamMate : UserControl
+    {
+        public TeamMate()
+        {
+            this.InitializeComponent();
+        }
+
+        #region Dependency Property Title
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(TeamMate), new PropertyMetadata(null));
+
+        #endregion
+
+
+
+        public string Email
+        {
+            get { return (string)GetValue(EmailProperty); }
+            set { SetValue(EmailProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Email.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmailProperty =
+            DependencyProperty.Register("Email", typeof(string), typeof(TeamMate), new PropertyMetadata(null));
+
+
+
+        #region Dependency Property Department
+
+
+
+        public string Department
+        {
+            get { return (string)GetValue(DepartmentProperty); }
+            set { SetValue(DepartmentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Department.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DepartmentProperty =
+            DependencyProperty.Register("Department", typeof(string), typeof(TeamMate), new PropertyMetadata(null));
+
+
+
+        #endregion
+
+        #region Dependency Property Image Source
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(TeamMate), new PropertyMetadata(null));
+        #endregion
+ 
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CSharp.UWP.App.Pages;
+using System.Drawing;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CSharp.UWP.App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+        /// <summary>
+        /// Navigation Item Selected, page loaded 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void NavTopLevelNav_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            Type _page = null;
+
+            
+            switch (args.InvokedItem)
+            {
+                case "Earbuds":
+                    
+                    _page = typeof(EarbudsPage);
+                    break;
+                case "Watchs":
+                    _page = typeof(WatchPage);
+                    break;
+                case "Thermostat":
+                    _page = typeof(ThermostatPage);
+                    break;
+                case "Laptops":
+                    _page = typeof(LaptopPage);
+                    break;
+                case "Accessories":
+                    _page = typeof(AccessoriesPage);
+                    break;
+            }
+            
+            NavView_Navigate(_page, args.RecommendedNavigationTransitionInfo);
+            NavTopLevelNav.SelectedItem = _page;
+        }
+        /// <summary>
+        /// Navigation for selected Nav Item
+        /// </summary>
+        /// <param name="_page"></param>
+        /// <param name="transitionInfo"></param>
+        private void NavView_Navigate(
+    Type _page,
+    Windows.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
+        {
+            
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(contentFrame.CurrentSourcePageType, _page))
+            {
+                contentFrame.Navigate(_page, null, transitionInfo);
+            }
+            else
+            {
+                contentFrame.Navigate(typeof(HomePage));
+                NavTopLevelNav.SelectedItem = typeof(HomePage);
+            }
+        }
+        /// <summary>
+        /// Top Menu Icon for viewing previous pages
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void NavTopLevelNav_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (contentFrame.CanGoBack)
+            {
+                contentFrame.GoBack();
+            }
+        }
+        /// <summary>
+        /// On Page Load, default page - HomePage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NavTopLevelNav_Loaded(object sender, RoutedEventArgs e)
+        {
+            contentFrame.Navigate(typeof(HomePage));
+            NavTopLevelNav.SelectedItem = typeof(HomePage);
+        }
+
+    }
+
+}
